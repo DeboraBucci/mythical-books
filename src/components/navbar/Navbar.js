159be8fd21f755a8f1774027b6b980149b2de432
Debora Bucci <dbucci@protonmail.com>
2022-06-28 15:46:33 -0300

    add navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">link</a>
+        </li>
+        <li>
+          <a href="/">link</a>
+        </li>
+        <li>
+          <a href="/">link</a>
+        </li>
+        <li>
+          <a href="/">link</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
