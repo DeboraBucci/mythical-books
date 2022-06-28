a13a78e5b8755aaadbfe51fb40f1b1df67c03952
Debora Bucci <dbucci@protonmail.com>
2022-06-28 15:57:35 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
+import Store from "./components/store/Store";
+import Error404 from "./components/errors/Error404";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <Home />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </React.Fragment>
   );
 }
