0506762a29fe03ac5424bbc11b96dcd168079e75
Debora Bucci <dbucci@protonmail.com>
2022-07-01 16:12:13 -0300

    add a 'Hero' with the title and slogan of the page to the 'Home' component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
+
 import Navbar from "../navbar/Navbar";
+import Hero from "./hero/Hero";
+
 import { homeLinks } from "../../data";
 
 const Home = () => {
   return (
     <div>
       <Navbar links={homeLinks} />
-      <h1>Mythical Books</h1>
+      <Hero />
     </div>
   );
 };
---
+++ src/components/home/hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero" id="hero">
+      <div className="hero__content">
+        <h1 className="hero__title">Mythical Books</h1>
+        <p className="hero__slogan">
+          Where your fantasies come alive <i className="fa-solid fa-broom"></i>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
