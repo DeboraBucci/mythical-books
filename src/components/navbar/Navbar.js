484faa2bff5d59b39a33c5765350d495366a8ab7
Debora Bucci <dbucci@protonmail.com>
2022-07-01 15:44:24 -0300

    make the 'Navbar' component reusable, and add classes to it using the BEM naming convention

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,28 @@
 import React from "react";
+import logo from "../../assets/logo.png";
 
-const Navbar = () => {
+const Navbar = ({ links }) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">link</a>
-        </li>
-        <li>
-          <a href="/">link</a>
-        </li>
-        <li>
-          <a href="/">link</a>
-        </li>
-        <li>
-          <a href="/">link</a>
-        </li>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <a href="#hero">
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "45px", height: "45px" }}
+          />
+        </a>
+      </div>
+
+      <ul className="navbar__list">
+        {links.map((link) => (
+          <li className="navbar__link" key={link.title}>
+            <a href={link.href}>
+              {link.title}{" "}
+              <i className={`${link.icon} navbar__icon--${link.iconClass}`}></i>
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
