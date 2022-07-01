84a1ee896df27f7b7a28ba2ef80f3bc5d3b61030
Debora Bucci <dbucci@protonmail.com>
2022-07-01 15:45:49 -0300

    add a navbar to the 'Home' component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navbar from "../navbar/Navbar";
+import { homeLinks } from "../../data";
 
 const Home = () => {
   return (
     <div>
+      <Navbar links={homeLinks} />
       <h1>Mythical Books</h1>
     </div>
   );
