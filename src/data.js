2d2abb6650b1caa15b0e331f85263c91ce29dccb
Debora Bucci <dbucci@protonmail.com>
2022-07-01 15:43:27 -0300

    create a file to store some of the page's data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const homeLinks = [
+  {
+    title: "Store",
+    href: "#",
+    icon: "fas fa-store",
+    iconClass: "store",
+  },
+  {
+    title: "How it Works",
+    href: "#works",
+    icon: "fas fa-dice-d20",
+    iconClass: "works",
+  },
+  {
+    title: "Benefits",
+    href: "#benefits",
+    icon: "far fa-gem",
+    iconClass: "benefits",
+  },
+  {
+    title: "Subscriptions",
+    href: "#subscriptions",
+    icon: "fas fa-dragon",
+    iconClass: "subscriptions",
+  },
+  {
+    title: "Log In",
+    href: "#",
+    icon: "fas fa-feather",
+    iconClass: "log",
+  },
+];
+
+export { homeLinks };
