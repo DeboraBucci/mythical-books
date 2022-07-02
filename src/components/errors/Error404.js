6c0a84c526eba208c23230fa3e0dadce9bdc007f
Debora Bucci <dbucci@protonmail.com>
2022-07-02 14:17:46 -0300

    transfer the 'opt' object to the 'data' file

--- src/components/errors/Error404.js
+++ src/components/errors/Error404.js
@@ -2,56 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import { opt } from "../../data";
 
 const Error404 = () => {
-  const opt = {
-    background: {
-      color: {
-        value: "#0a0016",
-      },
-    },
-    fpsLimit: 120,
-    particles: {
-      color: {
-        value: "#fff",
-      },
-      links: {
-        color: "#dfd3ff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 1,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 1000,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 0.1, max: 5 },
-      },
-    },
-    detectRetina: true,
-  };
-
   const particlesInit = async (main) => {
     await loadFull(main);
   };
--- src/data.js
+++ src/data.js
@@ -36,4 +36,52 @@ const homeLinks = [
   },
 ];
 
-export { homeLinks };
+const opt = {
+  background: {
+    color: {
+      value: "#0a0016",
+    },
+  },
+  fpsLimit: 120,
+  particles: {
+    color: {
+      value: "#fff",
+    },
+    links: {
+      color: "#dfd3ff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 1000,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 0.1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+
+export { homeLinks, opt };
