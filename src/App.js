16e0fdc27799b985ba0cc6c9490d79a1cf8ed0b0
Debora Bucci <dbucci@protonmail.com>
2022-07-02 14:04:23 -0300

    create a new component called 'Login' and add a route for it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./components/home/Home";
 import Store from "./components/store/Store";
+import Login from "./components/auth/Login";
 import Error404 from "./components/errors/Error404";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
+        <Route exact path="/login" element={<Login />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </React.Fragment>
