dedcbcea4614097e0a5d87865089fa4f2e421c8d
Debora Bucci <dbucci@protonmail.com>
2022-07-04 18:11:23 -0300

    create a component called 'InputGroup', which returns a dynamic and styled input field

---
+++ src/components/UI/InputGroup.js
@@ -0,0 +1,44 @@
+import { ErrorMessage, Field } from "formik";
+import TextError from "../UI/TextError";
+
+const InputGroup = ({ errors, icon, onKeyUp, value, name, type, onBlur }) => {
+  const findSpanElement = (e) =>
+    e.target.closest(".input-group").querySelector(".input-group__text");
+
+  const focusFieldHandler = (e) => {
+    const spanEl = findSpanElement(e);
+    spanEl.classList.add("filled");
+  };
+
+  const blurFieldHandler = (e) => {
+    const spanEl = findSpanElement(e);
+    if (!value) spanEl.classList.remove("filled");
+    onBlur(e);
+  };
+
+  return (
+    <div className="input-group__container">
+      <div className={`input-group ${errors && "input-group__error"}`}>
+        <label htmlFor={name} className={errors && "input-group__error"}>
+          <i className={`${icon} input-group__icon`}></i>
+          <span className="hidden">Email</span>
+        </label>
+        <Field
+          onFocus={focusFieldHandler}
+          onBlur={blurFieldHandler}
+          onKeyUp={onKeyUp}
+          type={type}
+          id={name}
+          name={name}
+          className="input-group__field"
+        />
+        <span className={`input-group__text ${value !== "" && "filled"}`}>
+          {name}
+        </span>
+      </div>
+      <ErrorMessage name={name} component={TextError} />
+    </div>
+  );
+};
+
+export default InputGroup;
