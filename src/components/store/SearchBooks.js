8e837f45d650e33fd91bc6a62c22538d9f529923
Debora Bucci <dbucci@protonmail.com>
2022-07-18 20:29:15 -0300

    create a new component called 'SearchBooks' which will display a form with a single input and a button, and will fetch the searched string from the Google's book api and retrieve a list of the matching books

---
+++ src/components/store/SearchBooks.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import fallbackImg from "../../assets/fallback-book-image.webp";
+
+const SearchBooks = ({ setBooks }) => {
+  const [searched, setSearched] = useState("");
+
+  const apiKey = "AIzaSyCfyJD3oJmISN4N-ANvOFW81JCl5LZE_Gk";
+  const link = `https://www.googleapis.com/books/v1/volumes?q=${searched}&key=${apiKey}`;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (searched !== "") {
+      fetch(link)
+        .then((res) => res.json())
+        .then((data) => {
+          const booksArr = [];
+          data.items.map((item) => {
+            console.log(item);
+            return booksArr.push({
+              title: item?.volumeInfo?.title,
+              authors: item?.volumeInfo?.authors || [],
+              image: item?.volumeInfo?.imageLinks?.thumbnail || fallbackImg,
+              maturity: item?.volumeInfo?.maturityRating,
+              id: item?.id,
+              rating: item?.volumeInfo?.averageRating,
+              ratingsCount: item?.volumeInfo?.ratingsCount,
+              price: item?.saleInfo?.retailPrice?.amount || "Out of Stock",
+            });
+          });
+
+          setBooks(booksArr);
+        });
+    } else {
+      setBooks([]);
+    }
+  };
+
+  const searchChangeHandler = (e) => setSearched(e.target.value.trim());
+
+  return (
+    <form className="store__search" onSubmit={submitHandler}>
+      <label htmlFor="search">Search</label>
+      <input
+        name="search"
+        id="search"
+        placeholder="Search books by title / author / publisher ..."
+        onChange={searchChangeHandler}
+      />
+      <button>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </form>
+  );
+};
+
+export default SearchBooks;
