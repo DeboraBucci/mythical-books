10a95e9c8b484e4aa202b9c30c417d7ac2130cb1
Debora Bucci <dbucci@protonmail.com>
2022-10-21 10:16:44 -0300

    add an array containing the subscription cards' information

--- src/data.js
+++ src/data.js
@@ -108,4 +108,101 @@ const opt = {
   detectRetina: true,
 };
 
-export { homeLinks, storeLinks, opt };
+const subscriptionCards = [
+  {
+    name: "Peasant",
+    icon: "fas fa-beer",
+    comment:
+      "be part of the heartwarming folk who likes to drink bear and pass out singing songs",
+    className: "peasant",
+    price: [0.99, 9.99],
+    benefits: {
+      devilery: false,
+      discounts: false,
+      chest: false,
+    },
+  },
+  {
+    name: "Blacksmith",
+    icon: "fas fa-gavel",
+    comment:
+      "be the one who uses his craftmanship to build items of great value",
+    className: "blacksmith",
+    price: [2.99, 29.99],
+    benefits: {
+      delivery: "Free delivery starting from $50",
+      discounts: false,
+      chest: false,
+      freeBook: false,
+    },
+  },
+  {
+    name: "Knight",
+    icon: "fas fa-chess-knight",
+    comment:
+      "ride ferociously into battle, without a hinch of doubt in your heart, and you may raise victorious!",
+    className: "knight",
+    price: [4.99, 47.99],
+    benefits: {
+      delivery: "Free delivery starting from $30",
+      discounts: "Up to 10% OFF in the store",
+      chest: "Iron Chest",
+      freeBook: false,
+    },
+  },
+  {
+    name: "Paladin",
+    icon: "fas fa-cross",
+    comment:
+      "be the sword and shield of the kingdom, swear to protect the truth, be the elite",
+    price: [9.99, 89.99],
+    className: "paladin",
+    benefits: {
+      delivery: "Free Delivery",
+      discounts: "Up to 25% OFF in the store",
+      chest: "Old Silver Chest",
+      freeBook: "Free book every 12 months",
+    },
+  },
+  {
+    name: "Wizard",
+    icon: "fas fa-hat-wizard",
+    comment: "so much power... do you feel it? It's growing deep inside of you",
+    className: "wizard",
+    price: [14.99, 149.99],
+    benefits: {
+      delivery: "Free Delivery",
+      discounts: "Up to 35% OFF in the store",
+      chest: "Saphire Chest",
+      freeBook: "Free book every 6 months",
+    },
+  },
+  {
+    name: "King or Queen",
+    icon: "fas fa-crown",
+    comment: "of royal blood, be a leader who knows the hearts of his people",
+    className: "king",
+    price: [19.99, 189.99],
+    benefits: {
+      delivery: "Free and Fast Delivery",
+      discounts: "Up to 50% OFF in the store",
+      chest: "Diamond Chest",
+      freeBook: "Free book each month",
+    },
+  },
+  {
+    name: "Ancient Dragon",
+    icon: "fas fa-dragon",
+    comment: "YOU... Impossible!!",
+    className: "dragon",
+    price: [29.99, 299.99],
+    benefits: {
+      delivery: "Free and Fast Delivery",
+      discounts: "30% to 70% OFF in the store",
+      chest: "Dragonscale chest",
+      freeBook: "Free book each month",
+    },
+  },
+];
+
+export { homeLinks, storeLinks, opt, subscriptionCards };
