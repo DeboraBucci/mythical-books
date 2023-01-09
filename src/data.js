8cf7c3f759aa612cdda3fa5aa022b5c7d130e703
Debora Bucci <dbucci@protonmail.com>
2023-01-09 17:57:41 -0300

    import and create an array of the images for the gallery

--- src/data.js
+++ src/data.js
@@ -1,3 +1,22 @@
+// Gallery images
+import Gallery1 from "./assets/gallery/gallery-1.webp";
+import Gallery2 from "./assets/gallery/gallery-2.webp";
+import Gallery3 from "./assets/gallery/gallery-3.webp";
+import Gallery4 from "./assets/gallery/gallery-4.webp";
+import Gallery5 from "./assets/gallery/gallery-5.webp";
+import Gallery6 from "./assets/gallery/gallery-6.webp";
+import Gallery7 from "./assets/gallery/gallery-7.webp";
+import Gallery8 from "./assets/gallery/gallery-8.webp";
+import Gallery9 from "./assets/gallery/gallery-9.webp";
+import Gallery10 from "./assets/gallery/gallery-10.webp";
+import Gallery11 from "./assets/gallery/gallery-11.webp";
+import Gallery12 from "./assets/gallery/gallery-12.webp";
+import Gallery13 from "./assets/gallery/gallery-13.webp";
+import Gallery14 from "./assets/gallery/gallery-14.webp";
+import Gallery15 from "./assets/gallery/gallery-15.webp";
+import Gallery16 from "./assets/gallery/gallery-16.webp";
+import Gallery17 from "./assets/gallery/gallery-17.webp";
+
 const homeLinks = [
   {
     title: "Store",
@@ -212,4 +231,24 @@ const subscriptionCards = [
   },
 ];
 
-export { homeLinks, storeLinks, opt, subscriptionCards };
+const galleryArr = [
+  { img: Gallery1, alt: "" },
+  { img: Gallery2, alt: "" },
+  { img: Gallery3, alt: "" },
+  { img: Gallery4, alt: "" },
+  { img: Gallery5, alt: "" },
+  { img: Gallery6, alt: "" },
+  { img: Gallery7, alt: "" },
+  { img: Gallery8, alt: "" },
+  { img: Gallery9, alt: "" },
+  { img: Gallery10, alt: "" },
+  { img: Gallery11, alt: "" },
+  { img: Gallery12, alt: "" },
+  { img: Gallery13, alt: "" },
+  { img: Gallery14, alt: "" },
+  { img: Gallery15, alt: "" },
+  { img: Gallery16, alt: "" },
+  { img: Gallery17, alt: "" },
+];
+
+export { homeLinks, storeLinks, opt, subscriptionCards, galleryArr };
