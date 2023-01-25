37fdb22e9e84611dfda7c66e544f1abe6123f285
Debora Bucci <dbucci@protonmail.com>
2023-01-25 15:11:15 -0300

    add some text and buttons to the gallery

--- src/components/home/gallery/Gallery.js
+++ src/components/home/gallery/Gallery.js
@@ -1,14 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { galleryArr } from "../../../data";
 import GalleryDecoration from "./GalleryDecoration";
 
 const Gallery = () => {
   return (
     <section className="gallery" id="gallery">
-      <div className="gallery__heading">
-        <h2>Our gallery</h2>
-        <p>Enjoy our member's pictures!</p>
+      <div className="gallery__text-container">
+        <div className="gallery__heading">
+          <h2>Our gallery</h2>
+        </div>
+
+        <div className="gallery__paragraphs">
+          <p>
+            This is the collection of memories of our precious members. In here,
+            you can see the amazing adventures everyone enrolled on.
+          </p>
+
+          <p>
+            By subscribing all the features of the gallery are made available
+            for you, such as uploading pictures, saving them, and more!
+          </p>
+
+          <p>To see the full gallery click here.</p>
+        </div>
+
+        <div className="gallery__cta">
+          <Link className="gallery__btn gallery__btn--1" to="register">
+            Register
+          </Link>
+          <a className="gallery__btn gallery__btn--2" href="#subscription">
+            Subscribe
+          </a>
+        </div>
       </div>
+
       <div className="gallery__imgs">
         {galleryArr.map((obj, i) => (
           <div className={`gallery__img-box gallery__img-box--${i + 1}`}>
