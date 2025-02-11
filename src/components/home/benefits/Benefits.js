c17eae2e07857dc9d7bad911b2c772026a866ce7
Debora Bucci <dbucci@protonmail.com>
2025-02-11 11:32:42 -0300

    use the SectionHeader and BenefitsItem components

--- src/components/home/benefits/Benefits.js
+++ src/components/home/benefits/Benefits.js
@@ -1,29 +1,23 @@
 import React, { useContext } from "react";
-import LanguageContext from "../../../context/language-context";
-import { benefitsContent, titles } from "../../../data";
+import LanguageContext from "../../../context/language-context.js";
+import { benefitsContent, titles } from "../../../data.js";
+import BenefitsItem from "./BenefitsItem.tsx";
+import SectionHeader from "../../UI/SectionHeader.tsx";
 
 const Benefits = () => {
   const languageCtx = useContext(LanguageContext);
 
   return (
     <section className="benefits" id="benefits">
-      <div className="benefits__heading section-heading">
-        <h2>{titles[languageCtx.language].benefits.main}</h2>
-        <p>{titles[languageCtx.language].benefits.subtitle}</p>
-      </div>
+      <SectionHeader
+        styles="benefits__heading"
+        title={titles[languageCtx.language].benefits.main}
+        subtitle={titles[languageCtx.language].benefits.subtitle}
+      />
+
       <ul className="benefits__list">
-        {benefitsContent[languageCtx.language].map(({ title, icon, text }) => (
-          <li className="benefits__item" key={title}>
-            <div className="benefits__item-heading">
-              <h3>{title}</h3>
-              <i className={icon}></i>
-            </div>
-            {text.map((item, i) => (
-              <React.Fragment key={`benefit-number--${i + 1}`}>
-                {item}
-              </React.Fragment>
-            ))}
-          </li>
+        {benefitsContent[languageCtx.language].map((item) => (
+          <BenefitsItem {...item} />
         ))}
       </ul>
     </section>
