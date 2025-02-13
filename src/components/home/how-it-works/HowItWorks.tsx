f13dba2af75efd886f46f5c62ad9e3f7be9386be
Debora Bucci <dbucci@protonmail.com>
2025-02-13 14:15:42 -0300

    create a file for storing the information of the HowItWorks component, and use it there

--- src/components/home/how-it-works/HowItWorks.tsx
+++ src/components/home/how-it-works/HowItWorks.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import LanguageContext from "../../../context/language-context";
 
-import { howItWorksContent } from "../../../data";
 import Step from "./Step";
 import { titles } from "data/general-data";
+import { howItWorksContent } from "data/how-it-works-data";
 
 const HowItWorks = () => {
-  const languageCtx = useContext(LanguageContext);
-  const { title, subtitle } = titles[languageCtx.language].howItWorks;
+  const { language } = useContext(LanguageContext);
+  const { title, subtitle } = titles[language].howItWorks;
 
   return (
     <section className="works" id="works">
@@ -17,20 +17,18 @@ const HowItWorks = () => {
       </div>
 
       <ul className="works__steps">
-        {(howItWorksContent as any)[languageCtx.language].map(
-          (step: any, i: number) => (
-            <Step
-              key={step.title}
-              title={step.title}
-              number={i + 1}
-              img={step.img}
-            >
-              {step.text.map((content: any, i: number) => (
-                <p key={`${step.title}__paragraph--${i}`}>{content}</p>
-              ))}
-            </Step>
-          )
-        )}
+        {howItWorksContent[language].map((step: any, i: number) => (
+          <Step
+            key={step.title}
+            title={step.title}
+            number={i + 1}
+            img={step.img}
+          >
+            {step.text.map((content: any, i: number) => (
+              <p key={`${step.title}__paragraph--${i}`}>{content}</p>
+            ))}
+          </Step>
+        ))}
       </ul>
     </section>
   );
