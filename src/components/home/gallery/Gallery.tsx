ada0be8d3e47ab56e20531903671e015c1e522d2
Debora Bucci <dbucci@protonmail.com>
2025-02-13 14:03:40 -0300

    create a file for storing the images and text of the gallery section

--- src/components/home/gallery/Gallery.tsx
+++ src/components/home/gallery/Gallery.tsx
@@ -1,32 +1,24 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import LanguageContext from "../../../context/language-context";
-import { galleryArr } from "../../../data";
 import GalleryDecoration from "./GalleryDecoration";
 import { titles } from "data/general-data";
+import { galleryTxt, galleryImages } from "data/gallery-section-data";
 
 const Gallery = () => {
-  const languageCtx = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
   return (
     <section className="gallery" id="gallery">
       <div className="gallery__text-container">
         <div className="gallery__heading section-heading">
-          <h2>{titles[languageCtx.language].gallery.title}</h2>
+          <h2>{titles[language].gallery.title}</h2>
         </div>
 
         <div className="gallery__paragraphs">
-          <p>
-            This is the collection of memories of our precious members. In here,
-            you can see the amazing adventures everyone enrolled on.
-          </p>
-
-          <p>
-            By subscribing all the features of the gallery are made available
-            for you, such as uploading pictures, saving them, and more!
-          </p>
-
-          <p>To see the full gallery click here.</p>
+          {galleryTxt[language].map((text) => (
+            <p>{text}</p>
+          ))}
         </div>
 
         <div className="gallery__cta">
@@ -40,7 +32,7 @@ const Gallery = () => {
       </div>
 
       <div className="gallery__imgs">
-        {galleryArr.map((obj, i) => (
+        {galleryImages.map((obj, i) => (
           <div
             key={Math.random().toString()}
             className={`gallery__img-box gallery__img-box--${i + 1}`}
---
+++ src/data/gallery-section-data.tsx
@@ -0,0 +1,51 @@
+import Gallery1 from "../assets/gallery/gallery-1.webp";
+import Gallery2 from "../assets/gallery/gallery-2.webp";
+import Gallery3 from "../assets/gallery/gallery-3.webp";
+import Gallery4 from "../assets/gallery/gallery-4.webp";
+import Gallery5 from "../assets/gallery/gallery-5.webp";
+import Gallery6 from "../assets/gallery/gallery-6.webp";
+import Gallery7 from "../assets/gallery/gallery-7.webp";
+import Gallery8 from "../assets/gallery/gallery-8.webp";
+import Gallery9 from "../assets/gallery/gallery-9.webp";
+import Gallery10 from "../assets/gallery/gallery-10.webp";
+import Gallery11 from "../assets/gallery/gallery-11.webp";
+import Gallery12 from "../assets/gallery/gallery-12.webp";
+import Gallery13 from "../assets/gallery/gallery-13.webp";
+import Gallery14 from "../assets/gallery/gallery-14.webp";
+import Gallery15 from "../assets/gallery/gallery-15.webp";
+import Gallery16 from "../assets/gallery/gallery-16.webp";
+import Gallery17 from "../assets/gallery/gallery-17.webp";
+
+const galleryImages = [
+  { img: Gallery1, alt: "" },
+  { img: Gallery2, alt: "" },
+  { img: Gallery3, alt: "" },
+  { img: Gallery4, alt: "" },
+  { img: Gallery5, alt: "" },
+  { img: Gallery6, alt: "" },
+  { img: Gallery7, alt: "" },
+  { img: Gallery8, alt: "" },
+  { img: Gallery9, alt: "" },
+  { img: Gallery10, alt: "" },
+  { img: Gallery11, alt: "" },
+  { img: Gallery12, alt: "" },
+  { img: Gallery13, alt: "" },
+  { img: Gallery14, alt: "" },
+  { img: Gallery15, alt: "" },
+  { img: Gallery16, alt: "" },
+  { img: Gallery17, alt: "" },
+];
+
+const galleryTxt = {
+  english: [
+    "This is the collection of memories of our precious members. In here, you can see the amazing adventures everyone enrolled on.",
+    "By subscribing all the features of the gallery are made available for you, such as uploading pictures, commenting, and more!",
+  ],
+
+  spanish: [
+    "Esta es la colección de memorias de nuestros preciados miembros. Aquí podrás ver las increíbles aventuras en las que todos están.",
+    "Al subscribirte, todas las características de la galería se te habilitarán, como subir fotos, comentar, y más!",
+  ],
+};
+
+export { galleryImages, galleryTxt };
