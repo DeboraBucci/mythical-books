72a8668fbe1ec0ba8e19f5f68bb93ff686873b3b
Debora Bucci <dbucci@protonmail.com>
2025-02-14 15:38:43 -0300

    add error handling for the fetching functions

--- src/api/book-api.ts
+++ src/api/book-api.ts
@@ -4,39 +4,53 @@ import { BookInterface } from "types/books";
 const apiAddress = "https://localhost:7036/api";
 
 export const getBooks = async (): Promise<BookInterface[]> => {
-  const res = await axios.get(`${apiAddress}/Books`);
-  return res.data;
+  try {
+    const res = await axios.get(`${apiAddress}/Books`);
+    return res.data;
+  } catch (err) {
+    return [];
+  }
 };
 
 export const getSearchedBooks = async (
   searched: string
 ): Promise<BookInterface[]> => {
-  const res = await axios.get(`${apiAddress}/Books/search/${searched}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${apiAddress}/Books/search/${searched}`);
+    return res.data;
+  } catch (err) {
+    return [];
+  }
 };
 
-export const getBookById = async (id: string) => {
-  const { data } = await axios.get(`${apiAddress}/Books/${id}`);
+export const getBookById = async (
+  id: string
+): Promise<BookInterface | null> => {
+  try {
+    const { data } = await axios.get(`${apiAddress}/Books/${id}`);
 
-  const bookData: BookInterface = {
-    id: data.id,
-    stock: data.stock,
-    language: data.language,
-    title: data.title,
-    categories: data.categories,
-    image: data.image,
-    description: data.description,
-    authors: data.authors,
-    ratingCount: data.ratingCount,
-    averageRating: data.averageRating,
-    pages: data.pages,
-    publishers: data.publishers,
-    publishedYear: data.publishedYear,
-    price: data.price,
-    currency: "USD",
-    isbn10: data.isbN10,
-    isbn13: data.isbN13,
-  };
+    const bookData: BookInterface = {
+      id: data.id,
+      stock: data.stock,
+      language: data.language,
+      title: data.title,
+      categories: data.categories,
+      image: data.image,
+      description: data.description,
+      authors: data.authors,
+      ratingCount: data.ratingCount,
+      averageRating: data.averageRating,
+      pages: data.pages,
+      publishers: data.publishers,
+      publishedYear: data.publishedYear,
+      price: data.price,
+      currency: "USD",
+      isbn10: data.isbN10,
+      isbn13: data.isbN13,
+    };
 
-  return bookData;
+    return bookData;
+  } catch (err) {
+    return null;
+  }
 };
