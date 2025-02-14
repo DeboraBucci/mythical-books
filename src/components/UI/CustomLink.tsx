525c868082ed6cc2cf3f058ba02f72e562688906
Debora Bucci <dbucci@protonmail.com>
2025-02-14 17:50:35 -0300

    for the anchor tag links, prevent the default navigation, search for the section you want to travel to, and scroll it into view

--- src/components/UI/CustomLink.tsx
+++ src/components/UI/CustomLink.tsx
@@ -10,11 +10,26 @@ const CustomLink: React.FC<CustomLinkProps> = ({
   linkType = "anchor",
   att,
   children,
-}) =>
-  linkType === "anchor" ? (
-    <a href={att}>{children}</a>
+}) => {
+  const handleScroll = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    targetId: string
+  ) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return linkType === "anchor" ? (
+    <a href={att} onClick={(e) => handleScroll(e, att)}>
+      {children}
+    </a>
   ) : (
     <Link to={att}>{children}</Link>
   );
+};
 
 export default CustomLink;
