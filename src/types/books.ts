69fb81e9b501d8e6afe41bf57cb3b87882a997d8
Debora Bucci <dbucci@protonmail.com>
2025-02-14 12:57:55 -0300

    create a file and add interfaces for the books

---
+++ src/types/books.ts
@@ -0,0 +1,50 @@
+interface Author {
+  id: number;
+  name: string;
+  fuller_name?: string;
+  bio?: string;
+  birth_date: Date;
+  death_date?: Date;
+}
+
+interface Publisher {
+  id: number;
+  name: string;
+}
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+export interface BookInterface {
+  id: number;
+  ratingCount?: number;
+  averageRating?: number;
+  pages?: number;
+  stock: number;
+  publishedYear?: number;
+  weight?: number;
+  price: number;
+  title: string;
+  description?: string;
+  isbn10?: string;
+  isbn13?: string;
+  image?: string;
+  language: string;
+  physicalFormat?: string;
+  authors: Author[];
+  publishers: Publisher[];
+  categories: Category[];
+}
+
+export interface BookCardInterface {
+  id: number;
+  ratingCount?: number;
+  averageRating?: number;
+  stock: number;
+  price: number;
+  title: string;
+  image?: string;
+  authors: Author[];
+}
