283e7aac1ec05c766b15c4472af147285aa66676
Debora Bucci <dbucci@protonmail.com>
2025-02-17 14:33:15 -0300

    remove the BookDetail and add a * to the store's route to allow for nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import { useContext, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./components/home/Home";
-import Store from "./components/store/Store";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Error404 from "./components/errors/Error404";
 
 import ThemeContext from "./context/theme-context";
-import BookDetail from "./components/store/books/BookDetail";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import StoreRoutes from "./components/store/Store";
 
 function App() {
   const themeCtx = useContext(ThemeContext);
@@ -32,10 +31,9 @@ function App() {
     <div className={themeCtx.theme}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
+        <Route path="/store/*" element={<StoreRoutes />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/store/book/:id" element={<BookDetail />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
