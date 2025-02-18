4fff0dba795793ad508cdae05bc863ab2c6552e3
Debora Bucci <dbucci@protonmail.com>
2025-02-18 17:27:05 -0300

    add title, loop through the Cart Context's array of books, and display a CartItem for each book

--- src/components/store/cart/Cart.tsx
+++ src/components/store/cart/Cart.tsx
@@ -1,5 +1,45 @@
+import CartItem from "./CartItem";
+import { useContext } from "react";
+import { CartContext } from "context/CartProvider";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartCtx = useContext(CartContext);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <div>
+        <h2>Shopping Cart</h2>
+
+        <ul>
+          {cartCtx.books.map(
+            ({
+              id,
+              title,
+              physicalFormat,
+              authors,
+              unitPrice,
+              quantity,
+              discountPercentage,
+              image,
+            }) => (
+              <CartItem
+                id={id}
+                title={title}
+                bookFormat={physicalFormat}
+                authors={authors}
+                originalPrice={unitPrice}
+                units={quantity}
+                discountPercentage={discountPercentage}
+                cover={image}
+              />
+            )
+          )}
+        </ul>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Cart;
