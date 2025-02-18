015f3e2355bd656955024534ee05887cefdf2bc6
Debora Bucci <dbucci@protonmail.com>
2025-02-18 11:26:20 -0300

    create a component for displaying a title of a book and its authors

---
+++ src/components/store/cart/CartItemHeader.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+interface CartItemHeaderProps {
+  title: string;
+  authors: string[];
+}
+
+const CartItemHeader: React.FC<CartItemHeaderProps> = ({ title, authors }) => {
+  const formatAuthors = () => {
+    let authorsStr = "";
+
+    authors.forEach((author, i) => {
+      authorsStr += author;
+      if (i < authors.length - 1) authorsStr += ", ";
+    });
+
+    return authorsStr;
+  };
+
+  return (
+    <CartItemHeaderDiv>
+      <h3>{title}</h3>
+      <p>by {formatAuthors()}</p>
+    </CartItemHeaderDiv>
+  );
+};
+
+export default CartItemHeader;
+
+const CartItemHeaderDiv = styled.div`
+  width: 20rem;
+
+  * {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  h3 {
+    font-family: "Quintessential", cursive;
+    font-size: 2.4rem;
+    color: #2c2c2c;
+  }
+
+  p {
+    font-size: 1.6rem;
+    color: #848484;
+  }
+`;
