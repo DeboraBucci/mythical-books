7c42f993aa9d6d51ebe58fb69f4867b32b8ad2a4
Debora Bucci <dbucci@protonmail.com>
2025-02-24 16:09:32 -0300

    create a component for displaying the items of the cart in a list

---
+++ src/components/store/cart/CartItems.tsx
@@ -0,0 +1,46 @@
+import { CartContext } from "context/CartProvider";
+import { useContext } from "react";
+import CartItem from "./CartItem";
+import styled from "styled-components";
+
+const CartItems = () => {
+  const cartCtx = useContext(CartContext);
+
+  return (
+    <ItemList>
+      {cartCtx.books.map(
+        ({
+          id,
+          title,
+          physicalFormat,
+          authors,
+          unitPrice,
+          quantity,
+          discountPercentage,
+          image,
+        }) => (
+          <CartItem
+            id={id}
+            title={title}
+            bookFormat={physicalFormat}
+            authors={authors}
+            originalPrice={unitPrice}
+            units={quantity}
+            discountPercentage={discountPercentage}
+            cover={image}
+          />
+        )
+      )}
+    </ItemList>
+  );
+};
+
+export default CartItems;
+
+const ItemList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+`;
