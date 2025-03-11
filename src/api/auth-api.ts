fb5e7655d0b026f4ed5bd20fe7599765d0cc3b9f
Debora Bucci <dbucci@protonmail.com>
2025-03-11 10:23:00 -0300

    add login request

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -14,3 +14,16 @@ export const registerUser = async (user: any): Promise<string | undefined> => {
     return undefined;
   }
 };
+
+export const loginUser = async (
+  email: string,
+  password: string
+): Promise<string | undefined> => {
+  try {
+    const res = await axios.post(`${address}/login`, { email, password });
+
+    return res.data.token;
+} catch (err) {
+    return undefined;
+  }
+};
