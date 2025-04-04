aad5310846d00679dc86c57c2f060beec377704c
Debora Bucci <dbucci@protonmail.com>
2025-04-03 21:58:28 -0300

    add styles with styled components, and check if books has a length greater than zero before rendering list

--- src/components/store/books/Bookshelf.tsx
+++ src/components/store/books/Bookshelf.tsx
@@ -1,30 +1,54 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import BookCard from "./book-card/BookCard";
 import { BooksContext } from "context/BooksProvider";
+import Pagination from "../../../components/UI/Pagination";
+import styled from "styled-components";
 
 const Bookshelf = () => {
   const { books } = useContext(BooksContext);
 
+  useEffect(() => {}, []);
+
   return (
-    <div className="store__bookshelf">
+    <RootComponent className="store__bookshelf">
       {books.length === 0 && <p>No books found.</p>}
-      {books.map((book) => {
-        return (
-          <BookCard
-            physicalFormat={book.physicalFormat}
-            title={book.title}
-            image={book.image}
-            authors={book.authors}
-            key={book.id}
-            id={book.id}
-            stock={book.stock}
-            averageRating={book.averageRating}
-            ratingCount={book.ratingCount}
-            price={book.price}
-          />
-        );
-      })}
-    </div>
+
+      {books?.length > 0 && (
+        <ul>
+          {books.map((book) => {
+            return (
+              <BookCard
+                physicalFormat={book.physicalFormat}
+                title={book.title}
+                image={book.image}
+                authors={book.authors}
+                key={book.id}
+                id={book.id}
+                stock={book.stock}
+                averageRating={book.averageRating}
+                ratingCount={book.ratingCount}
+                price={book.price}
+              />
+            );
+          })}
+        </ul>
+      )}
+      <Pagination />
+    </RootComponent>
   );
 };
 export default Bookshelf;
+
+const RootComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 7rem 4rem;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 4rem;
+  }
+`;
