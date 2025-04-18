68fa94faacd96bebeab25c6150dd7b814ca0d98e
Debora Bucci <dbucci@protonmail.com>
2025-04-18 15:34:25 -0300

    make pagination correctly display pages depending on the selected page

--- src/components/store/pagination/Pagination.tsx
+++ src/components/store/pagination/Pagination.tsx
@@ -2,10 +2,15 @@ import { BooksContext } from "context/BooksProvider";
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 
+const MAX_SCOPE = 5;
+
 const Pagination = () => {
   const booksCtx = useContext(BooksContext);
 
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
   const [selected, setSelected] = useState(1);
+  const [indexes, setIndexes] = useState([0, MAX_SCOPE]);
 
   useEffect(() => {
     if (selected > 1 && selected < 5) {
@@ -14,25 +19,63 @@ const Pagination = () => {
   }, [selected]);
 
   const onNumberChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelected(+e.currentTarget.value.trim());
+    const newSelected = +e.currentTarget.value.trim();
+
+    if (newSelected < 1 || newSelected > arr.length) {
+      return;
+    }
+
+    setSelected(newSelected);
+
+    if (newSelected >= 1 && newSelected <= MAX_SCOPE) {
+      setIndexes([0, MAX_SCOPE]);
+    } else if (newSelected > arr.length - MAX_SCOPE) {
+      setIndexes([arr.length - MAX_SCOPE, arr.length]);
+    } else {
+      setIndexes([newSelected - MAX_SCOPE, newSelected]);
+    }
+  };
+
+  const decrementSelectedHandler = () => {
+    const newSelected = selected - 1;
+
+    if (selected > 0) setSelected((prev) => prev - 1);
+
+    // Se decrementa los indices hasta [0, MAX_SCOPE]
+    if (indexes[0] >= 0) {
+      setIndexes((prev) => [prev[0] - 1, prev[1] - 1]);
+    }
+  };
+
+  const incrementSelectedHandler = () => {
+    const newSelected = selected + 1;
+
+    if (selected < arr.length) setSelected((prev) => prev + 1);
+
+    if (
+      newSelected > MAX_SCOPE &&
+      newSelected <= arr.length &&
+      newSelected > indexes[1]
+    ) {
+      setIndexes((prev) => [prev[0] + 1, prev[1] + 1]);
+    }
   };
 
   return (
     <RootContainer>
       <div className="main-container">
-        <Arrow
-          disabled={selected == 1}
-          onClick={() => setSelected((prev) => (prev > 1 ? prev - 1 : prev))}
-        >
+        <Arrow disabled={selected == 1} onClick={decrementSelectedHandler}>
           <i className="fa-solid fa-chevron-left" />
         </Arrow>
 
-        <div>
-          <p>...</p>
-        </div>
+        {selected >= MAX_SCOPE && (
+          <div>
+            <p>...</p>
+          </div>
+        )}
 
         <ul>
-          {[1, 2, 3, 4, 5].map((num) => (
+          {arr.slice(indexes[0], indexes[1]).map((num) => (
             <li
               key={num}
               onClick={() => setSelected(num)}
@@ -43,13 +86,15 @@ const Pagination = () => {
           ))}
         </ul>
 
-        <div>
-          <p>...</p>
-        </div>
+        {selected < arr.length && (
+          <div>
+            <p>...</p>
+          </div>
+        )}
 
         <Arrow
-          disabled={selected == 5}
-          onClick={() => setSelected((prev) => (prev < 5 ? prev + 1 : prev))}
+          disabled={selected == arr.length}
+          onClick={incrementSelectedHandler}
         >
           <i className="fa-solid fa-chevron-right" />
         </Arrow>
@@ -63,7 +108,7 @@ const Pagination = () => {
           value={selected}
           onChange={onNumberChangeHandler}
         />
-        <p>of 380 pages</p>
+        <p>of {booksCtx.totalPages} pages</p>
       </InputContainer>
     </RootContainer>
   );
